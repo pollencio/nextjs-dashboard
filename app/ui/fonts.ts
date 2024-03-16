36e25debd497059afb96cf430156cc1cba16ba36
Yeliana Torres <yeliana.torres@pluto.tv>
2024-03-15 21:11:38 -0500

    add custom fonts with next/fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Nunito_Sans, Quicksand } from 'next/font/google';
+
+export const nunitoSans = Nunito_Sans({ subsets: ['latin'] });
+export const quicksand = Quicksand({ subsets: ['latin'] });
