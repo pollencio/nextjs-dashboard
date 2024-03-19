bf1902e38c0716a3200cf25b852c47398aec65f5
Yeliana Andrea Torres Medina <yelis124@gmail.com>
2024-03-19 00:17:17 +0000

    consume database in charts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Nunito_Sans, Quicksand } from 'next/font/google';
 
 export const nunitoSans = Nunito_Sans({ subsets: ['latin'] });
-export const quicksand = Quicksand({ subsets: ['latin'] });
+export const quicksand = Quicksand({ subsets: ['latin'], weight: ['500'] });
